49d6de2fe5fffbc3b8624f2cf7557789a4f48c99
Vladimir <vladimir.fodorow@gmail.com>
2019-04-16 16:02:23 +0700

    Initial commit

---
+++ src/main/java/SpringWithPetr/Application.java
@@ -0,0 +1,13 @@
+package SpringWithPetr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
